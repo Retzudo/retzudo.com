3105af13012f15a529cd23faabf4fb12fddfed18
Retzudo <christoph.willinger@gmail.com>
2016-01-14 11:19:46 +0100

    icon stuff

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 
 
 app = Flask(__name__)
@@ -28,14 +28,20 @@ def browserconfig():
     )
 
 
-@app.route('/apple-touch-icon<size>.png', defaults={'size': ''})
 @app.route('/apple-touch-icon.png')
-def apple_touch_icons(size=None):
-    print('######### apple-touch-icon{}.png'.format(size))
-    if size:
-        filename = 'apple-touch-icon{}.png'.format(size)
-    else:
-        filename = 'apple-touch-icon.png'
+@app.route('/apple-touch-icon-57x57.png')
+@app.route('/apple-touch-icon-60x60.png')
+@app.route('/apple-touch-icon-72x72.png')
+@app.route('/apple-touch-icon-76x76.png')
+@app.route('/apple-touch-icon-114x114.png')
+@app.route('/apple-touch-icon-120x120.png')
+@app.route('/apple-touch-icon-144x144.png')
+@app.route('/apple-touch-icon-152x152.png')
+@app.route('/apple-touch-icon-180x180.png')
+@app.route('/mstile-70x70.png')
+@app.route('/mstile-144x144.png')
+def touch_icons():
+    filename = request.path.lstrip('/')
     return send_from_directory(
         os.path.join(app.root_path, 'static/images'),
         filename,
@@ -43,7 +49,6 @@ def apple_touch_icons(size=None):
     )
 
 
-@app.route('/mstile-<size>.png')
 def ms_tiles(size):
     return send_from_directory(
         os.path.join(app.root_path, 'static/images'),
