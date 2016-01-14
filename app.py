5fa56e0c03e4f01b2b0cd239e1176a2ba6476ea1
Retzudo <christoph.willinger@gmail.com>
2016-01-14 11:02:39 +0100

    Icons + link list

--- app.py
+++ app.py
@@ -27,5 +27,15 @@ def browserconfig():
         mimetype='application/xml'
     )
 
+
+@app.route('/apple-touch-icon-<size>.png')
+def apple_touch_icons(size):
+    return send_from_directory(
+        os.path.join(app.root_path, 'static/images'),
+        'apple-touch-icon-{}.png'.format(size),
+        mimetype='image/png'
+    )
+
+
 if __name__ == '__main__':
     app.run(debug=True)
